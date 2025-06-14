6d04301d8ce98aaa72fb6a9f4b7a18e1682b7c5d
Pallavi C <info@joindevops.com>
2025-06-14 07:18:28 +0530

    jenkins

---
+++ hello.py
@@ -0,0 +1,85 @@
+#print("Hello World")
+
+# # Variables
+# 1. Integer
+# 2. Float/double
+# 3. Strings
+# 4. Boolean
+# 5. List
+# 6. Tuples
+# 7. Dictionary
+# 8. Sets
+
+
+# print(10 + 20)
+# res = 10 +20
+# print("Result: ", res)
+# print("Memory address: ", id(res))
+# print("Data type: ", type(res))
+
+"""
+ This is a block comment
+"""
+
+# This is a line comment
+
+# a = 10.23
+# b = 20.32
+# res = a + b 
+# print("Result: ", res, "type: ", type(res))
+
+# sample_str = "This Is A String"
+# print(sample_str, type(sample_str))
+# print(dir(sample_str))
+
+'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill'
+
+# print(sample_str.capitalize())
+# print(sample_str.casefold())
+# print(sample_str.center())
+# print(sample_str.swapcase())
+# print(sample_str.split(' '))
+# print(sample_str.find('A'))
+# print(sample_str.index('T'))
+# print(sample_str.index('s'))
+
+# sample_char = sample_str[5]
+# print(sample_char)
+# print(len(sample_str))
+
+# a = True
+# b = False
+
+# print(a and b) # output will be false
+# print(a or b) # output will be true
+
+a = 2
+b = 2
+
+print(a + b) # Addition
+print(a - b) # substraction
+print(a * b) # multiplication
+print(a / b) # division
+print(a % b) # modulo divison returns remainder
+print(a // b) # Integer divison
+print(a ** b) # expontiation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
