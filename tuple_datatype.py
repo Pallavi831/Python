d25c57a87d553689f94a412113acc4e52e08a9bb
Pallavi C <info@joindevops.com>
2025-06-14 22:49:02 +0530

    jenkins

---
+++ tuple_datatype.py
@@ -0,0 +1,29 @@
+# t = (1, 3, 5, "true", [10, 30, 50])
+# # print(t)
+
+# # t1 = t[-1]
+# # print(t1)
+
+# # t[0]= 100
+# # # tipule is a inmutuable data type we cannot change once you updated details
+# # print(t)
+# # print(dir(t))
+
+# """
+# 'count', 'index'
+
+# """
+# print(t.index(5))
+
+# t = (1, 2, 3, 3, 3, 2, 2, 3)
+# print(t.count(2))
+
+# unpacking
+#t = (1, 2)
+t =[1, 2]
+
+# t1 = t[0]
+# t2 = t[1]
+t1, t2 = t
+print(t1, t2)
+
