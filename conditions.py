22a5580218e24d6744fc42d930ed49d4ece61694
Pallavi C <info@joindevops.com>
2025-06-15 18:15:17 +0530

    jenkins

--- conditions.py
+++ conditions.py
@@ -1,11 +1,31 @@
-# if, elseif, else
-num1 = 50
+# # if, elseif, else
+# num1 = 500
 
-if (num1 > 50) and (num1 <= 100):
-    print("Number is less than or equal to 100 and number is greater than 50")
-elif numl <= 50:
+# if (num1 > 50) and (num1 <= 100):
+#     print("Number is less than or equal to 100 and number is greater than 50")
 
-    print("Number is less than or equal to 50")
+# elif num1 <= 50:
+#     print("Number is less than or equal to 50")
 
+# else:
+#     print("Number is grater than 100")
+
+
+# if True: 
+#     print("I am evaluated")
+
+# if 1:
+#     print("I am evaluted")
+
+
+# Anything that is non-zero is condidered as true
+# if -1:
+#     print("I am evaluted")
+
+if 0:
+    print("I am evaluted")
 else:
-    print("Number is grater than 100")
+    print("Zero")
+
+
+
---
+++ loops.py
@@ -0,0 +1,10 @@
+# for, for-else , while , while-else
+
+# continue, break
+
+l = [10, 20, 30 , "abcd", False]
+
+# Loop Variable or itreable
+
+for ele in l:
+    print(ele)
