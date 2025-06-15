599ab43434011aa2238f01ef8c9f94363acdef97
Pallavi C <info@joindevops.com>
2025-06-15 07:50:52 +0530

    jenkins

---
+++ dict_datatype.py
@@ -0,0 +1,19 @@
+# d  = {'k': 123, (1, 2, 3): 123, (1, 2, 3, 4, 5): list(str(123))}
+# print(d[(1, 2, 3, 4, 5)])
+# print(dir(d))
+
+"""
+'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values'
+
+"""
+# print(d.keys())
+# print(d.values())
+# print(d.items())
+# # ([('k', 123), ((1, 2, 3), 123), ((1, 2, 3, 4, 5), ['1', '2', '3'])])
+# # dict is an mutable datatype
+
+# d['k'] = 1234
+# print(d)
+
+
+
---
+++ dict_set_datatype.py
@@ -0,0 +1,17 @@
+s = {'a', 'a', 'b', 'c'}
+# print(s)
+
+# s[0]= 'abc'
+# print(s)
+
+# TypeError: 'set' object does not support item assignment
+
+# print(dir(s))
+"""
+'add', 'clear', 'copy', 'difference', 'difference_update', 'discard', 'intersection', 'intersection_update', 'isdisjoint', 'issubset', 'issuperset', 'pop', 'remove', 'symmetric_difference', 'symmetric_difference_update', 'union', 'update'
+
+"""
+set = {'apple', 'banana', 'mango'}
+print('banana' in set)
+print('goa' in set)
+print('apple' not in set)
